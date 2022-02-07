61ceec9481847fc74b1f2e47b39ed2641f01e63e
haddyadnan <haddyadnan@gmail.com>
2022-02-07 16:10:10 +0100

    created marketing app

--- crmapp/crmapp/settings.py
+++ crmapp/crmapp/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'marketingapp'
 ]
 
 MIDDLEWARE = [
@@ -135,7 +136,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
