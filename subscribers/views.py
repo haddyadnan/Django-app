db5d3f8bf1eea57bbf3ed48a6c168ac10fd8dc8b
haddyadnan <haddyadnan@gmail.com>
2022-02-27 10:06:21 +0100

    Created the subscriber view

--- subscribers/views.py
+++ subscribers/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
+from django.http import HttResponseRedirect
+from .forms import SubscriberForm
 # Create your views here.
+
+def SubscriberForm(request, template='subscriber/subscriber_new.html'):
+    if request.method == 'POST':
+        form = SubscriberForm(request.Post)
+        if form.is_valid():
+            # Unpack form values
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            email = form.cleaned_data['email']
+
+            #Create user record
+            user = User(username=username, email=email)
+            user.set_password(password)
+            user.save()
+
+
+            return HttResponseRedirect('/success')
+
+    else:
+        form = SubscriberForm()
+
+    return render(request, template, {'form':form})
